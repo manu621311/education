4ea12ec6837d0215b11277d20b5cf998f9c7d4cb
Manumanglam <manurockmanglam@gmail.com>
2020-06-04 11:02:31 +0530

    After 1st phase

--- education_project/settings.py
+++ education_project/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7s(^qgr@=hb4f#8wfnvxtvt*ykwwd&t5#2jqnl6v$r4kcjg9oz'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
